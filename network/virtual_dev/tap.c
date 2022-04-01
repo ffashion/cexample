4ce614cbc272829f1d4e62d96366c19e8328093d
ffashion <helloworldffashion@gmail.com>
2022-04-01 16:13:53 +0800

    update tap

--- network/virtual_dev/tap.c
+++ network/virtual_dev/tap.c
@@ -32,6 +32,7 @@ int tap_alloc(int flag) {
     if (fd < 0) {
         return -1;
     }
+    memset(&ifr, 0, sizeof(struct ifreq));
     ifr.ifr_flags = flag;
     strcpy(ifr.ifr_name, "tap0");
 
@@ -40,20 +41,24 @@ int tap_alloc(int flag) {
         return -1;
     }
 
-    return 0;
+    return fd;
 }
-
+// step 1: sudo ifconfig tap0 192.168.1.2/24
+// step 2: ping or curl the other ips, example 192.168.1.3
 
 int	main(int argc, char **argv) {
     int fd, n;
-    char buf[10];
+    char buf[48];
     fd = tap_alloc(IFF_TUN | IFF_NO_PI);
     if(fd < 0) {
         perror("tap alloc");
         return -1;;
     }
+    //set ip address 
+    system("sudo ifconfig tap0 192.168.1.2/24");
     for (;;) {
         n = read(fd, buf, sizeof(buf));
+        printf("read %d byte\n", n);
         hexToString((unsigned char *)buf, NULL, n, 1);
     }
     
