86c46546715fdcaf4943efe3ffd61e4766bbb65f
ffashion <helloworldffashion@gmail.com>
2022-04-21 17:44:05 +0800

    update tap.c

--- network/tap/tap.c
+++ network/tap/tap.c
@@ -1,18 +1,38 @@
-//have ip 
-#include <sys/ioctl.h>
+//2 layer 
+#include <linux/if.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
+#include <errno.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <net/ethernet.h>
+#include <linux/if_ether.h>
+#include <sys/socket.h>
+#include <linux/if_packet.h>
 #include <linux/if_tun.h>
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <string.h>
-#include <stdlib.h>
-#include <linux/if.h>
+#include <fcntl.h>
+
+#define ETH_HDRLEN 14  // Ethernet header length
+#define IP4_HDRLEN 20  // IPv4 header length
+#define ICMP_HDRLEN 8  // ICMP header length for echo request, excludes data
+
+void debug_output_mac(char *you, unsigned char *addr) {
+  
+    fprintf(stderr, "[%s] %02x:%02x:%02x:%02x:%02x:%02x\n",you,addr[0],
+          addr[1], addr[2], addr[3], addr[4], addr[5]);
+}
+
+
+
 void hexToString(unsigned char *hex, char *str, int len, int output) {
   if (str == NULL) {
     str = malloc(2 * len) ;
@@ -51,18 +71,31 @@ int tap_alloc(int flag) {
 int	main(int argc, char **argv) {
     int fd, n;
     char buf[1024];
+    struct ethhdr *recv_ethhdr;
+    struct ip *recv_iphdr;
+    struct icmp *recv_icmphdr;
     //set TAP Flag, get IP data
-    fd = tap_alloc(IFF_TAP | IFF_NO_PI);
+    fd = tap_alloc(IFF_TAP | IFF_NO_PI); //no have packet infomation
     if(fd < 0) {
         perror("tap alloc");
         return -1;;
     }
+    recv_ethhdr = (struct ethhdr *)buf;
+    recv_iphdr = (struct ip *) (buf + ETH_HDRLEN);
+    recv_icmphdr = (struct icmp *) (buf + ETH_HDRLEN + IP4_HDRLEN);
+
     //set ip address 
     system("sudo ifconfig tap0 192.168.1.2/24");
     for (;;) {
         n = read(fd, buf, sizeof(buf));
-        printf("read %d byte\n", n);
-        hexToString((unsigned char *)buf, NULL, n, 1);
+        
+        debug_output_mac("source mac", recv_ethhdr->h_source);
+        debug_output_mac("dest mac:", recv_ethhdr->h_dest);
+        if(recv_ethhdr->h_proto == htons(ETH_P_IP)) {
+            printf("this is a ip packet\n");
+            printf("src ip: %s\n", inet_ntoa(recv_iphdr->ip_src));
+            printf("dst ip: %s\n", inet_ntoa(recv_iphdr->ip_dst));
+        }
     }
     close(fd);
     return 0;
