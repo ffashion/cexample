49746f3ae6e7d7586169cabbd890f8d324616e75
ffashion <helloworldffashion@gmail.com>
2022-11-08 01:08:42 -0500

    fix style: more readable

--- hyperscan/hyperscan.c
+++ hyperscan/hyperscan.c
@@ -7,6 +7,21 @@
 #include  <stdlib.h>
 #include  <stdlib.h>
 
+static int 
+ch_match_handler(unsigned int id,
+                                unsigned long long from,
+                                unsigned long long to,
+                                unsigned int flags,
+                                unsigned int size,
+                                const ch_capture_t *captured,
+                                void *ctx);
+int
+hs_event_handler(unsigned id,unsigned long long from,unsigned long long to,
+unsigned int flags,void *ctx);
+
+int hs_pattern_match(struct dict *dict, struct str *hys);
+typedef int (*pattern_match)(struct dict *dict, struct str *hys);
+
 struct array {
     void *elts;
     int  nelts;
@@ -27,12 +42,14 @@ struct str {
 
 struct pattern {
     struct str str;
+    int id;
     void *ctx;
 };
 
 struct dict {
     struct array *patterns;
-    struct hs_db *hdb;
+    struct hs_db hdb;
+    pattern_match match;
 };
 
 struct request {
@@ -77,9 +94,10 @@ struct dict *read_dict(char *file) {
     assert(dict);
 
     assert(fp);
-
+    int id = 0;
     for (;;) {
         char buf[PATTERN_MAX_SIZE];
+        int len;
         memset(buf, 0, sizeof(buf));
         if (fgets(buf, sizeof(buf), fp) == NULL) {
             if (feof(fp)) {
@@ -89,12 +107,20 @@ struct dict *read_dict(char *file) {
                 return NULL;
             }
         }
+        //remove last's LF
+        len = strlen(buf);
+        buf[len -1] = '\0';
+
         struct pattern *pattern = (struct pattern *)array_push(dict->patterns);
-        pattern->str.data  =  strdup(buf);
+        pattern->str.data  =  strdup(buf); //need remove \n
+        
         assert(pattern->str.data);
         // memcpy(pattern->str, buf, strlen(buf) + 1);
-        pattern->str.len = strlen(buf);
+        pattern->str.len = len - 1;
+        pattern->id = id++;
     }
+
+    dict->match = hs_pattern_match;
     return dict;
 }
 
@@ -128,16 +154,25 @@ struct request *read_requests(char *file) {
 }
 
 
-int 
-event_handler(unsigned id,unsigned long long from,unsigned long long to,
-unsigned int flags,void *ctx);
+int hs_pattern_match(struct dict *dict, struct str *hys) {
+    if (dict->hdb.ishs) {
+        //FIXME
+    }else {
+        // HS_SCAN_TERMINATED
+        if (ch_scan((ch_database_t *)dict->hdb.db, hys->data, hys->len, 0, (ch_scratch_t *)dict->hdb.scratch, ch_match_handler, NULL, dict) != HS_SUCCESS) {
+            printf("ch_scan error\n");
+            return -1;
+        }   
+    }
+    return 0;
+}
 
-int try_prepare_hs(const char **expressions, int npattern, struct dict *dict) {
+int try_prepare_hs(const char **expressions, int npattern, unsigned *ids, struct dict *dict) {
     hs_compile_error_t *hs_cerr;
     hs_database_t *hsdb;
     hs_scratch_t  *hs_scratch;
     
-    if (hs_compile_multi(expressions, 0, 0, npattern, HS_MODE_BLOCK, NULL, &hsdb, &hs_cerr) != HS_SUCCESS) {
+    if (hs_compile_multi(expressions, 0, ids, npattern, HS_MODE_BLOCK, NULL, &hsdb, &hs_cerr) != HS_SUCCESS) {
         hs_free_compile_error(hs_cerr);
         return -1;
     }
@@ -147,18 +182,19 @@ int try_prepare_hs(const char **expressions, int npattern, struct dict *dict) {
         hs_free_database(hsdb);
         return -1;
     }
-    dict->hdb->db = hsdb;
-    dict->hdb->scratch = hs_scratch;
-    dict->hdb->ishs = 1;
+    dict->hdb.db = hsdb;
+    dict->hdb.scratch = hs_scratch;
+    dict->hdb.ishs = 1;
     return 0;
 }
 
-int try_prepare_ch(const char **expressions, int npattern, struct dict *dict) {
+int try_prepare_ch(const char **expressions, int npattern, unsigned *ids, struct dict *dict) {
     ch_compile_error_t *ch_cerr;
     ch_database_t      *chdb;
     ch_scratch_t       *ch_scratch;
-    
-    if (ch_compile_multi(expressions, 0, 0, npattern, HS_MODE_BLOCK, NULL, &chdb, &ch_cerr) != HS_SUCCESS) {
+    // http://intel.github.io/hyperscan/dev-reference/api_constants.html#pattern-flags
+    if (ch_compile_multi(expressions, 0, ids, npattern, CH_MODE_GROUPS, NULL, &chdb, &ch_cerr) != HS_SUCCESS) {
+        printf("%s\n", ch_cerr->message);
         ch_free_compile_error(ch_cerr);
         return -1;
     }
@@ -166,9 +202,9 @@ int try_prepare_ch(const char **expressions, int npattern, struct dict *dict) {
         ch_free_database(chdb);
         return -1;
     }
-    dict->hdb->db = chdb;
-    dict->hdb->scratch = ch_scratch;
-    dict->hdb->ishs = 1;
+    dict->hdb.db = chdb;
+    dict->hdb.scratch = ch_scratch;
+    dict->hdb.ishs = 0;
     return 0;
 }
 
@@ -179,31 +215,64 @@ int main(int argc, char const *argv[]){
     struct request *r = read_requests((char *)"./requests.txt");
 
 
-    const char *expressions[1024];
-
+    const char *expressions[10240];
+    unsigned ids[10240] = {0};
     npattern =  dict->patterns->nelts;
     struct pattern *nds = (struct pattern *)dict->patterns->elts; //nd stand for needles
 
-    for (i = 0; i < dict->patterns->nelts; i++) {
-        expressions[i] = nds->str.data;
+for (i = 0; i < npattern; i++) {
+        expressions[i] = nds[i].str.data;
+        ids[i] = nds[i].id;
     }
 
-    if (try_prepare_hs(expressions, npattern, dict) == -1) {
-        printf("prepare hs error\n");
-    }else if (try_prepare_ch(expressions, npattern, dict) == -1) {
+    if (try_prepare_hs(expressions, npattern, ids, dict) == 0) {
+    }else if (try_prepare_ch(expressions,npattern, ids, dict) == -1) {
         printf("prepare ch error exit\n");
         return -1;
     }
     
+    struct str *hys = (struct str *)r->strs->elts;
+    nrequest = r->strs->nelts;
+
+    for (i = 0; i < nrequest; i++) {
+        if (dict->match(dict, &hys[i]) == 0) {
+            // printf("%s match!!!\n", hys[i].data);
+        }
+    }
 
     return 0;
 }
 
 
 int 
-event_handler(unsigned id,unsigned long long from,unsigned long long to,
+hs_event_handler(unsigned id,unsigned long long from,unsigned long long to,
 unsigned int flags,void *ctx){
-    printf("id %d from %llu :Match for pattern \"%s\" at offset %llu\n", id,from,(char *)ctx, to);
+    struct dict *dict = (struct dict *)ctx;
+
+    printf("id %d from %llu to %llu\n", id, from, to);
+
+
+
+    return true;
+}
+
+static int 
+ch_match_handler(unsigned int id,
+                                unsigned long long from,
+                                unsigned long long to,
+                                unsigned int flags,
+                                unsigned int size,
+                                const ch_capture_t *captured,
+                                void *ctx) {
+    // http://intel.github.io/hyperscan/dev-reference/api_files.html#c.match_event_handler
+    struct dict *dict = (struct dict *)ctx;
+    // [form, to)
+    char *data = (char *)ctx;
+
+    printf("id %d from %llu to %llu\n", id, from, to);
+    //we can't memset data here...
+
+    //0 stand for next match
     return 0;
 
 }
